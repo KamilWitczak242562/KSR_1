34b24468ee9b80c4427f369a298dbe1ff660dd1c
Kamil Witczak <222150@edu.p.lodz.pl>
2024-03-28 13:56:17 +0100

    Loading and init dictionaries finished

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,29 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+       Dictionaries dictionaries = new Dictionaries();
+       dictionaries.loadDictionary("places.txt", dictionaries.getCharacteristicPlaces());
+       dictionaries.loadDictionary("administration.txt", dictionaries.getAdministration());
+       dictionaries.loadDictionary("dates.txt", dictionaries.getYears());
+       dictionaries.loadDictionary("famous.txt", dictionaries.getFamousPeople());
+       dictionaries.loadDictionary("geographical.txt", dictionaries.getGeographical());
+       dictionaries.loadDictionary("holidays.txt", dictionaries.getHolidays());
+       dictionaries.loadDictionary("names.txt", dictionaries.getNames());
+       dictionaries.loadDictionary("towns.txt", dictionaries.getTowns());
+//       System.out.println(dictionaries.getAdministration());
+//       System.out.println(dictionaries.getGeographical());
+//       System.out.println(dictionaries.getFamousPeople());
+//       System.out.println(dictionaries.getHolidays());
+//       System.out.println(dictionaries.getFamousPeople());
+//       System.out.println(dictionaries.getCharacteristicPlaces());
+//       System.out.println(dictionaries.getNames());
+//       System.out.println(dictionaries.getYears());
+//       dictionaries.initCurrency();
+//       dictionaries.initStandards();
+//       dictionaries.initCountries();
+//       System.out.println(dictionaries.getCurrency());
+//       System.out.println(dictionaries.getStandards());
+//       System.out.println(dictionaries.getCountries());
+
     }
 }

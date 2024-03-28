a76e21a58216f470e75f6b77c4c74ce2bb8b7174
Kamil Witczak <222150@edu.p.lodz.pl>
2024-03-28 15:22:15 +0100

    Loading articles finished

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,16 +1,19 @@
 package org.example;
 
+import java.io.IOException;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-       Dictionaries dictionaries = new Dictionaries();
-       dictionaries.loadDictionary("places.txt", dictionaries.getCharacteristicPlaces());
-       dictionaries.loadDictionary("administration.txt", dictionaries.getAdministration());
-       dictionaries.loadDictionary("dates.txt", dictionaries.getYears());
-       dictionaries.loadDictionary("famous.txt", dictionaries.getFamousPeople());
-       dictionaries.loadDictionary("geographical.txt", dictionaries.getGeographical());
-       dictionaries.loadDictionary("holidays.txt", dictionaries.getHolidays());
-       dictionaries.loadDictionary("names.txt", dictionaries.getNames());
-       dictionaries.loadDictionary("towns.txt", dictionaries.getTowns());
+//       Dictionaries dictionaries = new Dictionaries();
+//       dictionaries.loadDictionary("places.txt", dictionaries.getCharacteristicPlaces());
+//       dictionaries.loadDictionary("administration.txt", dictionaries.getAdministration());
+//       dictionaries.loadDictionary("dates.txt", dictionaries.getYears());
+//       dictionaries.loadDictionary("famous.txt", dictionaries.getFamousPeople());
+//       dictionaries.loadDictionary("geographical.txt", dictionaries.getGeographical());
+//       dictionaries.loadDictionary("holidays.txt", dictionaries.getHolidays());
+//       dictionaries.loadDictionary("names.txt", dictionaries.getNames());
+//       dictionaries.loadDictionary("towns.txt", dictionaries.getTowns());
 //       System.out.println(dictionaries.getAdministration());
 //       System.out.println(dictionaries.getGeographical());
 //       System.out.println(dictionaries.getFamousPeople());
@@ -25,6 +28,14 @@ public class Main {
 //       System.out.println(dictionaries.getCurrency());
 //       System.out.println(dictionaries.getStandards());
 //       System.out.println(dictionaries.getCountries());
-
+        List<Article> ar;
+        try {
+            ar = ArticlesLoader.loadData("reut2-001.sgm", 2);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        for (Article article : ar) {
+            System.out.println(article.toString());
+        }
     }
 }
